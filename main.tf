34dca278373e61c9529302bf27ad7a73be967e9e
neil <neiltl@phildata.com>
2024-01-22 13:45:29 +0800

    Added network security group rule to an NSG

--- main.tf
+++ main.tf
@@ -37,6 +37,32 @@ resource "azurerm_subnet" "mtc-subnet" {
   virtual_network_name = azurerm_virtual_network.mtc-vn.name
   address_prefixes     = ["10.123.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "mtc-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "72.14.201.149/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+}
+
+
 #terraform init
 #terraform fmt
 #terraform plan
